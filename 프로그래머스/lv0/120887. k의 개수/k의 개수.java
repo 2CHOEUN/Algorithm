8a2325b2cc357b7b0094b171e5478da03d158ab1
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-03-02 22:55:20 +0900

    Update k의 개수.java

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -4,7 +4,7 @@ class Solution {
         int answer = 0;
       
         for(int z=i;z<=j;z++){
-              String gg = Integer.toString(z);
+            String gg = Integer.toString(z);
             String kk = Integer.toString(k);
             for(int x=0;x<gg.length();x++){
                 if(gg.charAt(x)==kk.charAt(0)){
