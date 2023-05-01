8fa7cc2d060e0beea5495331f165e8164ebd612f
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-05-01 18:33:15 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        int year = 2022;
+        for(int i=1;i<age;i++ ){
+            year --;
+        }
+        
+        return year;
+    }
+}
