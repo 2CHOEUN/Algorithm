b0eaffabd4e5f0810523c2b89adf75c18339cff5
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-05-03 19:39:56 +0900

    [unrated] Title: 수 조작하기 2, Time: 647.60 ms, Memory: 385 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int[] numLog) {
+    String answer = "";
+        
+        for(int i = 0;i<numLog.length-1;i++){
+            int num = numLog[i+1] - numLog[i];
+            switch(num){
+                case 1 : answer += "w"; break;
+                case -1 : answer += "s"; break;
+                case 10 : answer += "d"; break;
+                case -10 :answer += "a"; break;
+            
+            }
+        }
+
+        return answer;
+    }
+}
