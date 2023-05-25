97f5fdc6d2127450eff14b6d1df0b15d1a8b6ce7
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-05-25 15:53:31 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,12 +1,18 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int [5];
-        Arrays.sort(num_list);
-         for(int i = 0; i < answer.length; i++){
-             answer[i] = num_list[i];
-         }
+        for(int i = 0; i<num_list.length-1; i++){
+            for(int j = i+1; j <num_list.length; j++){
+                if(num_list[i] > num_list[j]){
+                    int temp = num_list[i];
+                    num_list[i] = num_list[j];
+                    num_list[j] = temp;
+                }
+            }
+        }
+        for(int i = 0; i < 5; i++){
+            answer[i] = num_list[i];
+        }
         return answer;
     }
 }
