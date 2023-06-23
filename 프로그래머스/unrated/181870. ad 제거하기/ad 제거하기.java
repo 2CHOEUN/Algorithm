43f588327a858bac43a65d191e4e9f0e99f37d79
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-06-23 22:29:18 +0900

    [unrated] Title: ad 제거하기, Time: 1.39 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList <String> arr = new ArrayList<String>();
+        
+        for(int i = 0; i < strArr.length; i++){
+            if(!strArr[i].contains("ad")){
+                arr.add(strArr[i]);
+            }
+        }
+        
+        String[] answer = new String [arr.size()];
+        
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
