ebb191eac844add355e60527b08358ca7affcdf3
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-06-28 11:05:34 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        
+        int answer = 0;
+
+        for(int i = idx; i < arr.length; i++){
+            if(arr[i] == 1){
+                answer = i;
+                break;
+            }else{
+                answer = -1;
+            }
+        }
+        
+        return answer;
+    }
+}
