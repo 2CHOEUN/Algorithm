6b3fd75c7fc3dc226fdb66302194e576016208f6
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-07-25 12:01:43 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] solution(String my_string) {
+        
+        int num = 0;
+        
+        for(char i = 'a'; i <='z'; i++){
+            num ++;
+        }
+        for(char i = 'A'; i <='Z'; i++){
+            num ++;
+        }
+        
+        int answer[] = new int [num];
+        
+        num = 0;
+        
+         for(int i = 'A'; i <= 'Z'; i++){
+            for(int j = 0; j < my_string.length(); j++){
+                if(i == my_string.charAt(j)){
+                    answer[num] ++;
+                }
+            }
+              num++;
+        }
+        
+        for(int i = 'a'; i <= 'z'; i++){
+            for(int j = 0; j < my_string.length(); j++){
+                if(i == my_string.charAt(j)){
+                    answer[num] ++;
+                }
+            }
+            num++;
+           }
+         
+        
+        return answer;
+    }
+}
