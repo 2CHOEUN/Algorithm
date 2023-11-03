f1c8dedf61fe28ccdf5e5326206d5d3be1ca423f
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-11-03 21:56:27 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0;i<name.length;i++){
+            map.put(name[i],yearning[i]);
+
+        }
+        int[] answer = new int[photo.length];
+
+        for(int i=0;i<photo.length;i++){
+            int sum = 0;
+            for(int j=0;j<photo[i].length;j++){
+                if(map.containsKey(photo[i][j])){
+                    sum += map.get(photo[i][j]);
+                }
+            }
+            answer[i] = sum;
+        }
+        
+        
+        return answer;
+    }
+}
