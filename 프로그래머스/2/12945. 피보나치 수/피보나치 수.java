0329f64bf7c574381266588411ef4ecdaa544b13
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-11-16 23:02:20 +0900

    [level 2] Title: 피보나치 수, Time: 2.36 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int n) {
-     int[] fib = new int[n + 1];
+     int[] F = new int[n + 1];
 
-        fib[0] = 0;
-        fib[1] = 1;
+        F[0] = 0;
+        F[1] = 1;
 
         for (int i = 2; i <= n; i++) {
-            fib[i] = (fib[i - 1] + fib[i - 2]) % 1234567;
+            F[i] = (F[i - 1] + F[i - 2]) % 1234567;
         }
 
-        return fib[n];
+        return F[n];
     }
 }
