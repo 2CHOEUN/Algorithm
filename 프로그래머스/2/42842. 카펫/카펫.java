ad068c9083eb76d54342a3a9f1bb31c42f8745f6
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2023-11-20 20:44:27 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int total = brown + yellow;
+     
+         for(int width=3;width<=Math.sqrt(total);width++){
+             if(total % width ==0){
+                 int height = total / width;
+                 if((width - 2) * (height - 2)==yellow){
+                     answer[0] = height;
+                     answer[1] = width;
+                    return answer;
+                 }
+             }
+
+         }
+        return answer;
+    }
+}
