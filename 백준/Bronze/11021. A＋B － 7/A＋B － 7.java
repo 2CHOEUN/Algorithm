790ef1c271ea885f35ce3047aaaa8468c335720e
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-01-08 21:20:30 +0900

    [Bronze V] Title: A+B - 7, Time: 260 ms, Memory: 18696 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+        for(int i=1; i<=T;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            System.out.println("Case #"+i+": " +(a+b));
+        }
+    }
+}
