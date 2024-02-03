1532fa0068e2915d7c93ecd089ef25aae8827fe8
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-02-03 18:00:00 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        if(price>=500000){
+         answer = price-(int)Math.round(price*0.2);
+        }else if(price>=300000){
+             answer = price-(int)Math.round(price*0.1);
+        }else if(price>=100000){
+            answer = price-(int)Math.round(price*0.05);
+        }else{
+            answer = price;
+        }
+
+        return answer;
+    }
+}
