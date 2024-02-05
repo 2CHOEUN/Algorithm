76e08002ce19ae4002fa7378a1ad549449143b25
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-02-05 21:48:07 +0900

    [Bronze V] Title: 문자와 문자열, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        
+        int i = sc.nextInt();
+        
+        System.out.print(S.charAt(i-1));
+    }
+}
