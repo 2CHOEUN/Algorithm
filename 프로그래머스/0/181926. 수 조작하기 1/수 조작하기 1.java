6dc84bdea69530289d1f2d9f2c9251e5c824c14e
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-02-29 21:36:02 +0900

    [level 0] Title: 수 조작하기 1, Time: 2.30 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        for(int i=0; i<control.length();i++){
+            if(control.charAt(i)=='w'){
+                n = n + 1;
+            }else if(control.charAt(i)=='s'){
+                n -= 1;
+            }else if(control.charAt(i)=='d'){
+                  n += 10;
+            }else{
+                n -= 10;
+            }
+        }
+        return n;
+    }
+}
