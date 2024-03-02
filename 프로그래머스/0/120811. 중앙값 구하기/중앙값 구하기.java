917c1b3b39f3318bd734a527d08bf90cdbc63e02
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-03-02 11:01:02 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.37 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+
+        int answer = 0;
+       Arrays.sort(array);
+        
+        answer = array[(array.length-1)/2];
+       
+        
+        return answer;
+    }
+}
