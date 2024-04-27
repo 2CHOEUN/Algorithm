d352c6b04ca6a6b4a5ae38c6895f0cb7f274e579
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-04-27 19:40:55 +0900

    [Bronze IV] Title: 기찍 N, Time: 1108 ms, Memory: 32392 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i= N; i >0; i--){
+            System.out.println(i);
+        }
+    }
+}
