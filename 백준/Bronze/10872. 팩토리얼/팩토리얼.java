a611060df584907d345470195842844208f67542
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-05-06 22:38:26 +0900

    [Bronze III] Title: 팩토리얼, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int result = 1;
+        for(int i = N; i  >= 1; i--){
+            result *= i;
+        }
+        System.out.println(result);
+
+
+    }
+}
