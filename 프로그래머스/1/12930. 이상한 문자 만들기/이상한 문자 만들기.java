3a3566257260b56ed9fd82b5b1191926fe33e172
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-07-30 00:21:10 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.15 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,19 +1,18 @@
 class Solution {
-    public String solution(String s) {
-       String answer = "";
-        int count = 0;
-        for(int i=0;i<s.length();i++){
-            if(Character.isWhitespace(s.charAt(i))){
-                answer += " ";
-                count = 0;
-            }else if(count % 2 ==0){
-                answer += Character.toUpperCase(s.charAt(i));
-                count++;
+      public String solution(String s) {
+        char [] chars = s.toCharArray();
+        int cnt = 0;
+        StringBuilder br = new StringBuilder();
+          for (char aChar : chars) {
+            cnt ++;
+            cnt = Character.isSpaceChar(aChar) ? 0: cnt ++;
+            if(cnt == 0){
+                br.append(" ");
+                continue;
             }else{
-                answer += Character.toLowerCase(s.charAt(i));
-                count++;
+                br.append(cnt % 2 == 0 ? Character.toLowerCase(aChar) : Character.toUpperCase(aChar));
             }
         }
-        return answer;
-    }
+        return br.toString();
+      }
 }
