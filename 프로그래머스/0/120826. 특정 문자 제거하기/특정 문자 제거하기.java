3a2b43b7b7be6ac1e5b48f708f6c790b3ce0882d
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-08-10 14:57:39 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 14.18 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+                String answer = "";
+
+       for(int i=0;i<my_string.length();i++){
+           for(int j=0; j<letter.length();j++){
+               if(my_string.charAt(i)!=letter.charAt(j)){
+                 answer += my_string.charAt(i);
+               }
+           }
+       }
+        
+        return answer;
+    }
+}
