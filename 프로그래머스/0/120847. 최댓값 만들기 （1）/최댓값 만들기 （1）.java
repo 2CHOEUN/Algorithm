c67614c1a1f14286f631ccd266c3c1d7e5d772e6
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-08-12 22:22:29 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.21 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        
+        for(int i=0;i<numbers.length-1;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                if(numbers[i]<numbers[j]){
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+              }
+          }
+     int answer = numbers[0]*numbers[1];
+     return answer;
+    }
+}
