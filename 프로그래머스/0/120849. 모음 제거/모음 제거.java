49cd630bf37b624bf3ed6547c95e009cea64e5e7
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-08-14 22:46:10 +0900

    [level 0] Title: 모음 제거, Time: 0.16 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string) {
  
-        String answer = my_string.replaceAll("[aeiou]","");
+        String answer = my_string.replaceAll("[aeiou]", "");
         
         return answer;
     }
