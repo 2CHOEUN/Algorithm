21c1a94a2e16ec5028fb14f758913ca9b11213fd
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-08-15 11:13:53 +0900

    [level 0] Title: 약수 구하기, Time: 0.79 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        
+        ArrayList <Integer> arr = new ArrayList<Integer> ();
+        
+        for(int i = 1; i <= n;i ++){
+            if(n % i == 0){
+                arr.add(i);
+            }
+        }
+        int [] answer = new int[arr.size()];
+        for(int i=0;i<answer.length;i++){
+            answer[i] = arr.get(i);
+        }
+        Arrays.sort(answer);
+       
+        
+        return answer;
+    }
+}
