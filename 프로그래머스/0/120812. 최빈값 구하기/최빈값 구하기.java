9bd79f731acc024bf8e2fa9aa25941edfd0e92b6
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-08-17 21:44:09 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.97 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array) {
+        
+       Arrays.sort(array);
+        
+        int maxCount = 0;
+        int currentCount = 1;
+        int mode = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] == array[i - 1]) {
+                currentCount++;
+            } else {
+                if (currentCount > maxCount) {
+                    maxCount = currentCount;
+                    mode = array[i - 1];
+                } else if (currentCount == maxCount) {
+                    mode = -1;
+                }
+                currentCount = 1;
+            }
+        }
+        if (currentCount > maxCount) {
+            mode = array[array.length - 1];
+        } else if (currentCount == maxCount) {
+            mode = -1;
+        }
+        return mode;
+    }
+}
