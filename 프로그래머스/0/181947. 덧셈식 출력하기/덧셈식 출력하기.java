c9214362330f3f20ac90aabb148d7fd1fc35c565
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-08-24 20:25:49 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 164.36 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -6,6 +6,6 @@ public class Solution {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.println(""+a + " + " + b + " = " + (a+b));
+        System.out.println(""+a + " + " + b + " = " + ( a + b ));
     }
 }
