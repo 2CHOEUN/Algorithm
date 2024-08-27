5fc410d08325a0dda5ecba0135ba6fc7eb3631ff
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-08-28 01:51:20 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 11.48 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
         String answer = "";
-        for(int i=0;i<my_string.length();i++){
+        for(int i=0;i < my_string.length(); i++){
             if(i==num1){
                 answer += my_string.charAt(num2);
             }else if(i==num2){
