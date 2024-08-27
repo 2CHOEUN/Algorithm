ab3abc0da09ebfc1317111db4001576c412bd3eb
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-08-27 23:04:16 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i = 0; i<num_list.length;i++){
             answer[i] = num_list[i];
         }
-        if(num_list[num_list.length-1] > num_list[num_list.length-2]){
+        if(num_list[num_list.length-1] > num_list[num_list.length - 2]){
             answer[answer.length-1] =  num_list[num_list.length-1] - num_list[num_list.length-2];
         }else{
             answer[answer.length-1] = (num_list[num_list.length -1]) * 2;
