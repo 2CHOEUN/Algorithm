c6ebf46c29d5a02098e29954b3bd80054903ebb9
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-08-27 23:02:59 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.09 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        
+        int[] answer = new int[num_list.length - n + 1];
+        ArrayList ans = new ArrayList();
+        
+        for(int i = n-1; i < num_list.length; i++){
+            ans.add(num_list[i]);
+        }
+        for(int i = 0; i < ans.size(); i++){
+            answer[i] = (int)ans.get(i);
+        }
+        return answer;
+    }
+}
