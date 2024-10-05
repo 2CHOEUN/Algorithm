f91f19dda8bd27bb744b4c03774f3f7e8e6d4d31
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-10-05 20:06:59 +0900

    [level 0] Title: 순서쌍의 개수, Time: 7.10 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -3,8 +3,8 @@ class Solution {
         
         int answer = 0;
         
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
+        for(int i = 1;i <= n; i++){
+            if(n % i == 0){
                 answer ++;
             }
             
