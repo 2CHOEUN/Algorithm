83c6bff19130402862ebd12645fdb0651088b7af
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-10-10 22:51:48 +0900

    [level 0] Title: 주사위 게임 3, Time: 1.01 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int answer = 0;
+        int num[] = {a, b, c, d};
+        Arrays.sort(num);
+
+        if (num[0] == num[1] && num[1] == num[2] && num[2] == num[3]) {
+            return 1111 * num[0];
+        } else if (num[0] == num[1] && num[1] == num[2]) {
+            return (10 * num[0] + num[3]) * (10 * num[0] + num[3]);
+        } else if (num[1] == num[2] && num[2] == num[3]) {
+            return (10 * num[3] + num[0]) * (10 * num[3] + num[0]);
+        } else if (num[0] == num[1]) {
+            if (num[2] == num[3]) {
+                return (num[0] + num[2]) * Math.abs(num[0] - num[2]);
+            } else {
+                return num[2] * num[3];
+            }
+        } else if (num[1] == num[2]) {
+            if (num[0] == num[3]) {
+                return (num[1] + num[3]) * Math.abs(num[1] - num[3]);
+            } else {
+                return num[0] * num[3];
+            }
+        } else if (num[2] == num[3]) {
+            if (num[0] == num[1]) {
+                return (num[2] + num[1]) * Math.abs(num[2] - num[1]);
+            } else {
+                return num[0] * num[1];
+            }
+        } else {
+            return num[0];
+        }
+    }
+}
