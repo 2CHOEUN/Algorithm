0a1e594cc560c9c1b4c37dff111b71aa71b2c99c
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-10-21 21:34:06 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(int start_num, int end_num) {
         int[] answer = new int [end_num - start_num +1];
         int count = 0;
-        for(int i=start_num ;i<=end_num;i++){
+        for(int i=start_num ;i <= end_num;i++){
             answer[count ++] = i;
          }
         return answer;
