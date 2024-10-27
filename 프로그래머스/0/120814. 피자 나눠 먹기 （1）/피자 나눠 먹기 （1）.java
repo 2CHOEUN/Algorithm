431fcee15a468f4ce0e1fcbbcbbb10f51b24b4b5
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-10-27 09:00:59 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        if(n % 7==0){
+        if(n % 7 == 0){
         answer = n / 7;
         }else{
             answer = n / 7 + 1;
