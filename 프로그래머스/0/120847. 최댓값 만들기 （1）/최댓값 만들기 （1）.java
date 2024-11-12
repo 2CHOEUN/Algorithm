ae24c6c9b043a100798f5950624c5714a97e2aae
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-11-12 23:22:47 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.13 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int[] numbers) {
         
         for(int i=0;i<numbers.length-1;i++){
-            for(int j=i+1;j<numbers.length;j++){
-                if(numbers[i]<numbers[j]){
+            for(int j=i+1;j < numbers.length;j++){
+                if(numbers[i] < numbers[j]){
                     int temp = numbers[i];
                     numbers[i] = numbers[j];
                     numbers[j] = temp;
