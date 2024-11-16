dff00fe78a54ee7640a6309d0671d9b32fabac65
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-11-16 20:40:20 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,8 +3,8 @@ class Solution {
     public ArrayList solution(int n) {
         ArrayList <Integer> answer = new ArrayList<Integer>();
         
-        for(int i=1;i<=n;i++){
-            if(i%2!=0){
+        for(int i = 1; i <= n;i++){
+            if(i % 2 != 0){
                 answer.add(i);
             }
      
