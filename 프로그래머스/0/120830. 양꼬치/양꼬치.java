5ff65ed529d23d4bb059f68cb2b1e11347b12662
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-11-19 23:22:22 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n, int k) {
      int answer = (n*12000)+(k*2000);
         
-        for(int i=10;i<=n;i=i+10){
+        for(int i=10;i <= n;i = i+10){
           answer = answer - 2000;
         }
         
