e0d0be9a3c58ecbbd74ede18313d5188d172577e
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-11-20 21:40:57 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         
         for(int i=1;i<=n;i++)
-            if(i%2==0){
+            if(i % 2 == 0){
                 answer +=i;
             }
         return answer;
