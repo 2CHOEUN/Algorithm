7404705f452c0f6bae43ef209b03d1a0a9583fd4
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-11-28 19:18:00 +0900

    [level 0] Title: 커피 심부름, Time: 0.81 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        for(int i=0;i<order.length;i++){
+        for(int i = 0;i<order.length;i++){
             if(order[i].contains("americano") || order[i].equals("anything")){
                 answer += 4500;
             }else if(order[i].contains("cafelatte")){
