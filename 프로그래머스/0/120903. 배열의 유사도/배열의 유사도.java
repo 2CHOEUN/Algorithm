f24a4026ed8b6052b27419e0ba1d255b4e48e476
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-11-30 18:13:50 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         
         for(int i=0;i<s1.length;i++){
-            for(int j=0; j<s2.length;j++){
+            for(int j = 0; j < s2.length;j++){
                 if(s1[i].equals (s2[j])){
                     answer++;
                 }
