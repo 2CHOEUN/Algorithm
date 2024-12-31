d53326e8f0902d1ff3420dd95819314e0a6574ce
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2024-12-31 23:22:11 +0900

    [Unrated] Title: 랜덤 게임~, Time: 100 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Unrated/10943. 랜덤 게임～/랜덤 게임～.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+class Main{
+    public static void main(String args[]){
+        Random random = new Random();
+        int n = random.nextInt(10) + 1;
+        System.out.println(6);
+    }
+}
