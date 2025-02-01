ef4204ef6261398984e19dd2bb98ec0b84e9d6b5
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2025-02-01 23:53:05 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.88 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -4,7 +4,7 @@ class Solution {
         StringBuilder answer = new StringBuilder();
         for(int i=0;i<food.length;i++){
             if(food[i] >= 2){
-                int n = food[i]/2;
+                int n = food[i] / 2;
                 for(int j=1;j<=n;j++){
                  answer.append(i);
                 }
