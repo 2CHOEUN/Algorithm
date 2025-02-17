5c8547ea37911a0b826aad921f1bf0bec6670ee6
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2025-02-17 23:28:24 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.55 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,16 +1,8 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int [num_list.length - 5];
-        
-        for(int i = 0; i < num_list.length-1; i++){
-            for(int j = i+1; j < num_list.length; j++){
-                if(num_list[i] > num_list[j]){
-                    int temp = num_list[i];
-                    num_list[i] = num_list[j];
-                    num_list[j] = temp;
-                }
-            }
-        }
+        Arrays.sort(num_list);
+        int[] answer = new int[num_list.length -5];
         
         for(int i = 0; i < answer.length; i++){
             answer[i] = num_list[i+5];
