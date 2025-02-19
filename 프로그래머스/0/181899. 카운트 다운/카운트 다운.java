7ab9877e32e1849070c6e3b9e7b387ee1d357c88
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2025-02-19 23:51:32 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[start_num - end_num +1];
-        int num = 0;
-        for(int i=start_num; i>=end_num;i--){
-            answer[num++] = i;
-        }
         
+        int[] answer = new int[start_num - end_num + 1];
+        int idx = 0;
+        for(int i = start_num; i >= end_num; i--){
+            answer[idx++] = i;
+        }
         return answer;
     }
 }
