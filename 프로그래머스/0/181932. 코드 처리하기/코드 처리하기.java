2025ea02110deacc7853ef47fda1ba01cf5820a8
2CHOEUN <121777500+2CHOEUN@users.noreply.github.com>
2025-03-14 22:45:17 +0900

    [level 0] Title: 코드 처리하기, Time: 18.33 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,24 +1,18 @@
 class Solution {
     public String solution(String code) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         int mode = 0;
-        
-        for(int i = 0; i < code.length(); i++){
-            if(mode == 0){
-                if(code.charAt(i) != '1' && i % 2 == 0){
-                    answer += code.charAt(i);
-                }else if(code.charAt(i) == '1'){
-                    mode = 1;
-                }
-            }else{
-                if(code.charAt(i) != '1' && i % 2 != 0){
-                     answer += code.charAt(i);
-                }else if(code.charAt(i) == '1'){
-                    mode = 0;
-                }
+        for (int i = 0; i < code.length(); i++) {
+            char current = code.charAt(i);
+            if (current == '1') {
+                mode = mode == 0 ? 1 : 0;
+                continue;
             }
-         } 
-        
-        return answer.isEmpty() ? "EMPTY" : answer;
+
+            if (i % 2 == mode) {
+                answer.append(current);
+            }
+        }
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
     }
 }
